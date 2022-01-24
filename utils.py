231a6e9003ed13f996bc573f877c3bb3c09ab185
Jackie-Ljq <liujiaqi.er@gmail.com>
2022-01-23 22:25:34 -0800

    bkbone dual_bn

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from effdet.object_detection import Det_Dual_BN
+import torch.nn as nn
+import torch
 def get_clip_parameters(model, exclude_head=False):
     if exclude_head:
         # FIXME this a bit of a quick and dirty hack to skip classifier head params
@@ -10,3 +12,28 @@ def set_advState(model, advState):
     for name, submodel in model.named_modules():
         if isinstance(submodel, Det_Dual_BN):
             submodel.advState = advState
+
+def convert_dual_bn(model):
+    model_output = model
+    if isinstance(model, nn.BatchNorm2d):
+        model_output = Det_Dual_BN(model.num_features, model.eps, 
+                    model.momentum, model.affine, model.track_running_stats)
+        if model.affine:
+            with torch.no_grad():
+                model_output.bn_clean.weight = model.weight
+                model_output.bn_clean.bias = model.bias
+                model_output.bn_adv.weight = model.weight
+                model_output.bn_adv.bias = model.bias
+                
+        model_output.bn_clean.running_mean = model.running_mean
+        model_output.bn_clean.running_var = model.running_var
+        model_output.bn_clean.num_batches_tracked = model.num_batches_tracked
+        model_output.bn_adv.running_mean = model.running_mean
+        model_output.bn_adv.running_var = model.running_var
+        model_output.bn_adv.num_batches_tracked = model.num_batches_tracked
+    
+    for name, child in model.named_children():
+        model_output.add_module(name, convert_dual_bn(child))
+    
+    del model
+    return model_output
